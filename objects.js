005f369bc2513c12d419e647d3e8b51a2202650f
impartial-cache-1887 <marciafreeman2011@gmail.com>
2019-10-06 16:50:24 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = { artistName: 'songTitle'};
+
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  var newSong = playlist.artistName = songTitle
+ return playlist += newSong
+}
+
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.artistName
+  return playlist
+}
